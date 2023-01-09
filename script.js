f24d327958f960a94ee667f8c7b9d8558e4d13fa
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-01-09 17:56:38 -0600

    Add mousemove event and display coordinates of mouse using clientX and clientY

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-document.addEventListener("touchmove", function(event) {
-    // Prevent the page from scrolling
-    event.preventDefault();
-  
-    // Get the position of the finger as it moves over the page
-    var touch = event.touches[0];
-  
-    // Get the position of the finger relative to the table
-    var rect = event.target.getBoundingClientRect();
-    var x = touch.clientX - rect.left;
-    var y = touch.clientY - rect.top;
-  
-    // Find the cell at the finger position
-    var cell = document.elementFromPoint(x, y);
-  
-    // Paint the cell
-    if (cell.nodeName == "TD") {
-      document.getElementById("mousePosition").innerHTML = toString(x) + "" + toString(y)
-        //cell.style.backgroundColor = currentColor;
-    }
-  });
+//document.getElementById("mousePosition")
+
+function mouseCoordinates(e) {
+    document.getElementById("mousePosition").innerHTML = "X" + e.clientX + "Y" + e.clientY
+}
+
+document.addEventListener("mousemove", mouseCoordinates)
+
   
