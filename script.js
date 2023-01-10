886ac96bb691001d0280f57c26201f843f0ef379
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-01-10 17:52:12 -0600

    Solve issue for mousemove  where clientX and clientY are not updated or output unresolved
    Issue was that the mouse cursor would turn into an I-beam for text input
    over many parts of the table. Setting table style to cursor:pointer keeps
    the cursor as a pointer, allowing clientX and clientY to output as desired

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-document.querySelector("body").addEventListener("touchstart", f)
-document.querySelector("body").addEventListener("touchmove", f)
-document.querySelector("body").addEventListener("touchend", f)
+//document.querySelector("body").addEventListener("mousedown", f)
+document.querySelector("body").addEventListener("mouseover", f)
+//document.querySelector("body").addEventListener("mouseup", f)
 
+//xPosition = clientX
+//yPosition = clientY
 function f(e) {
-document.getElementById("debugDisplay").innerHTML = e.touches + e.type
-console.log(e.touches, e.type)
+e.preventDefault()
+document.getElementById("debugDisplay").innerHTML = "X:" + e.clientX + "Y:" + e.clientY
+console.log("X:" + e.clientX + "Y:" + e.clientY)
+//document.getElementById("debugDisplay").innerHTML = e.touches + e.type
+//console.log(e.touches, e.type)
 }
