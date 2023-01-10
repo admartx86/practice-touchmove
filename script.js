49221fb7140b066eafb49aefedf059566ffb17fa
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-01-09 18:29:05 -0600

    .

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //document.getElementById("mousePosition")
 
-function mouseCoordinates(e) {
-e.preventDefault()
+function mouseCoordinates() {
     document.getElementById("mousePosition").innerHTML = "X" + e.clientX + "Y" + e.clientY
-
 }
 
 document.addEventListener("mousemove", mouseCoordinates)
-document.addEventListener("touchmove", mouseCoordinates)
 
+document.addEventListener("touchmove", function(event) {
+    mouseCoordinates(event);
+  });
   
