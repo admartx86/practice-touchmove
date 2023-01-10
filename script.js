4bc0741f75564e893ed056628effbc12fae87659
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-01-09 18:18:35 -0600

    Add preventDefault to prevent screen from scrolling when it shouldn't, for touchscreen users

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //document.getElementById("mousePosition")
 
 function mouseCoordinates(e) {
+e.preventDefault()
     document.getElementById("mousePosition").innerHTML = "X" + e.clientX + "Y" + e.clientY
+
 }
 
 document.addEventListener("mousemove", mouseCoordinates)
