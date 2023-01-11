f3dddb03a594028da11c978186a8a20ae7ba335f
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-01-10 21:56:00 -0600

    .

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-//document.querySelector("body").addEventListener("mousedown", f)
-//
 document.getElementById("touchArea").addEventListener("mousemove", f, {passive:false})
-//document.querySelector("body").addEventListener("mouseup", f)
 document.getElementById("touchArea").addEventListener("touchmove", f, {passive:false})
-//xPosition = clientX
-//yPosition = clientY
 function f(e) {
-e.preventDefault()
-document.getElementById("debugDisplay").innerHTML = "X:" + e.pageX + "Y:" + e.pageY
-console.log("X:" + e.pageX + "Y:" + e.pageY)
-//document.getElementById("debugDisplay").innerHTML = e.touches + e.type
-//console.log(e.touches, e.type)
-}
+    e.preventDefault()
+    document.getElementById("debugDisplay").innerHTML = "X:" + e.pageX + "Y:" + e.pageY
+    console.log("X:" + e.pageX + "Y:" + e.pageY)
+    document.getElementById("debugDisplay2").innerHTML = e.touches + e.type
+    console.log(e.touches, e.type)
+} 
