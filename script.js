f9fb167268fb4a253de10a96d34fa4f922b70e7c
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-01-10 22:03:12 -0600

    .

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//document.getElementById("touchArea").addEventListener("mousemove", f, {passive:false})
+document.getElementById("touchArea").addEventListener("mousemove", f, {passive:false})
 document.getElementById("touchArea").addEventListener("touchmove", f, {passive:false})
 function f(e) {
     e.preventDefault()
     document.getElementById("debugDisplay").innerHTML = "X:" + e.pageX + "Y:" + e.pageY
     console.log("X:" + e.pageX + "Y:" + e.pageY)
-    document.getElementById("debugDisplay2").innerHTML = e.touches[0] + e.type
+    document.getElementById("debugDisplay2").innerHTML = "e.touches[0]" + e.touches[0] + "e.type" + e.type
     console.log(e.touches, e.type)
 } 
